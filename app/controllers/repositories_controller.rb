9118759609cdd95752275c4f0344408313ebd016
Nurudeen <ebroyeem90@gmail.com>
2022-04-23 22:39:07 +0100

    Consume github api

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,11 +1,24 @@
+require 'uri'
+require 'net/http'
+
 class RepositoriesController < ApplicationController
   def index
   end
 
   def search
+    uri = URI("https://api.github.com/search/repositories?q=#{params[:search_term]}")
+    res = Net::HTTP.get_response(uri)
+    # puts res.body
+    body = res.body if res.is_a?(Net::HTTPSuccess)
+    @repositories = JSON.parse(body)["items"]
+
     respond_to do |format|
       format.turbo_stream do
-        render turbo_stream: turbo_stream.update("search_result", params[:search_term])
+        render turbo_stream: turbo_stream.update(
+          "search_result",
+          partial: "repositories/search_result",
+          locals: { repositories: @repositories }
+        )
       end
     end
   end
