187b835d642ebab65789d3598cc1ede88b6ab7a6
Nurudeen <ebroyeem90@gmail.com>
2022-04-24 12:44:07 +0100

    Render different partials based on search response

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -6,17 +6,22 @@ class RepositoriesController < ApplicationController
   end
 
   def search
-    uri = URI("https://api.github.com/search/repositories?q=#{params[:search_term]}")
-    res = Net::HTTP.get_response(uri)
-    # puts res.body
-    body = res.body if res.is_a?(Net::HTTPSuccess)
-    @repositories = JSON.parse(body)["items"]
+    if params[:search_term].blank?
+      partial = "repositories/no_search_term"
+    else
+      uri = URI("https://api.github.com/search/repositories?q=#{params[:search_term]}")
+      res = Net::HTTP.get_response(uri)
+      # puts res.body
+      body = res.body if res.is_a?(Net::HTTPSuccess)
+      @repositories = JSON.parse(body)["items"]
+      partial = @repositories.empty? ? "repositories/no_results" : "repositories/search_result"
+    end
 
     respond_to do |format|
       format.turbo_stream do
         render turbo_stream: turbo_stream.update(
           "search_result",
-          partial: "repositories/search_result",
+          partial: partial,
           locals: { repositories: @repositories }
         )
       end
